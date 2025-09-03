86565afc957c51f783fb4216d67dbec076169d75
Pratik Kedar <141951998+PratikDevelops@users.noreply.github.com>
2025-09-03 14:33:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
     port: 5173,
-    host: true, 
-    hmr: {
-      clientPort: 443, 
-  }
-})
+    allowedHosts: [
+      ".csb.app",
+      ".stackblitz.io",
+      ".gitpod.io",
+      ".loca.lt",
+      "localhost",
+      "127.0.0.1",
+    ],
+  },
+});
